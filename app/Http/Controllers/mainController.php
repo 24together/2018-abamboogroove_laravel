d4e9137579a14199a90ac70e8b68975f4a89cd3c
minyeoungKim <to__get___her@naver.com>
2018-11-15 23:50:15 +0900

    Update app/Http/Controllers/mainController.php

--- app/Http/Controllers/mainController.php
+++ app/Http/Controllers/mainController.php
@@ -10,20 +10,6 @@ class mainController extends Controller
         return view('index');
     }
     public function secret_write(){
-//        require_once("MemberDao.php");
-//        require_once("tools.php");
-//
-//        session_start();
-//
-//        $uid = isset($_SESSION["uid"])?$_SESSION["uid"]:"";
-//        /*로그인 정보 가져오기*/
-//        $mdao = new MemberDao();
-//        $member = $mdao->getMember($uid);
-//        /*디비에서 찾아보기*/
-//        if(!$member){
-//            errorBack("[$uid] 로그인 해주세요.");
-//            exit();
-//        }
-//        return view('write_form')
+
     }
 }
