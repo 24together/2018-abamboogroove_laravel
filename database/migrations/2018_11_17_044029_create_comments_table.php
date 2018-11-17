7fccd609983607f865ef357435235a349cd152ff
minyeoungKim <to__get___her@naver.com>
2018-11-17 14:09:27 +0900

    Create 2018_11_17_044029_create_comments_table.php

---
+++ database/migrations/2018_11_17_044029_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('num');
+            $table->unsignedInteger('content_num');
+            $table->foreign('content_num')->references('num')->on('boards')->onDelete('cascade');
+            $table->string('writer');
+            $table->string('id');
+            $table->integer('like')->default(0);
+            $table->text('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
