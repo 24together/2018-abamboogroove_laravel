e443eb172c7debca0ffddd4b1611a29a4d800a66
minyeoungKim <to__get___her@naver.com>
2018-11-18 16:37:12 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,27 @@ Route::get('/', 'mainController@main');/*접속했을 때 메인만 보이려면
 Route::get('/member/login','loginController@login');
 Route::get('/member/join','loginController@join');
 Route::get('/member/update','loginController@update');
+
 Route::get('/secret/write','writeController@secretWrite');
 Route::get('/secret/board','writeController@secretBoard');
 Route::get('/secret/view/{num}','writeController@secretView');
+
 Route::get('/free/write','writeController@freeWrite');
 Route::get('/free/board','writeController@freeBoard');
 Route::get('/free/view/{num}','writeController@freeView');
-Route::get('/kakao','KakaoLoginController@index');
+
+Route::get('/kakao','KakaoLoginController@index');/*카카오 로그인 관련*/
 Route::get('/kakao/login','KakaoLoginController@redirectToProvider');
 Route::get('/kakao/login/callback','KakaoLoginController@handleProviderCallback');
 
 
 Route::post('/write','writeController@write');
 //카테고리 값 받아 글쓰기
+Route::post('/star_up/{num}/{category}','writeController@star');
+//게시글 별점 부여
+Route::post('/image_upload','writeController@summernote');
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
