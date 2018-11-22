5ef2a0fb165ddf6f8fc5d0b548c98df5474f63e9
minyeoungKim <to__get___her@naver.com>
2018-11-22 21:52:09 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,25 +13,28 @@
 Route::get('welcome', function (){
     return view('welcome');
 });
+//로그인관리
 Route::get('/', 'mainController@main');/*접속했을 때 메인만 보이려면*/
 Route::get('/member/login','loginController@login');
 Route::get('/member/join','loginController@join');
 Route::get('/member/update','loginController@update');
-
+//익명 게시판
 Route::get('/secret/write','writeController@secretWrite');
 Route::get('/secret/board','writeController@secretBoard');
 Route::get('/secret/view/{num}','writeController@secretView');
-
+Route::get('/secret/modify/{num}','writeController@secretModify');
+//자유 게시판
 Route::get('/free/write','writeController@freeWrite');
 Route::get('/free/board','writeController@freeBoard');
 Route::get('/free/view/{num}','writeController@freeView');
-
+//카카오
 Route::get('/kakao','KakaoLoginController@index');/*카카오 로그인 관련*/
 Route::get('/kakao/login','KakaoLoginController@redirectToProvider');
 Route::get('/kakao/login/callback','KakaoLoginController@handleProviderCallback');
 
-
+Route::post('/modify/{num}','writeController@modify');
 Route::post('/write','writeController@write');
+Route::post('/delete/{num}/{category}','writeController@delete');
 //카테고리 값 받아 글쓰기
 Route::post('/star_up/{num}/{category}','writeController@star');
 //게시글 별점 부여
