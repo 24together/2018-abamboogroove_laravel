1b4c23862bf123c5027fcc73216ff641b55931f7
minyeoungKim <to__get___her@naver.com>
2018-11-22 21:51:51 +0900

    Update writeController.php

--- app/Http/Controllers/writeController.php
+++ app/Http/Controllers/writeController.php
@@ -25,6 +25,29 @@ class writeController extends Controller
         return redirect($board_name.'/view/'.$board->num)->with('message','글이 정상적으로 등록되었습니다 !');
 
     }
+    public function delete($num,$category){
+        Board::where('num','like',$num)->delete();
+        if($category ==1){
+            $board_name="secret";
+        }else if($category ==2) {
+            $board_name="free";
+        }
+        return redirect($board_name.'/board');
+    }
+    public function modify(Request $request,$num){
+        $title = $request->title;
+        $content = $request->content_;
+        if($request->category ==1){
+            $category = "secret";
+        }else if($request->category ==2){
+            $category = "free";
+        }
+
+        Board::where('num',$num)->update(['title'=>$title,'content'=>$content]);
+        return redirect($category.'/view/'.$num)->with('message','글 수정을 하였습니다.');
+    }
+
+    /////secretboard//////////////////////
     public function secretWrite(Request $request){
         return view('secretboard.secret-write-form');
     }
@@ -33,21 +56,28 @@ class writeController extends Controller
         $msgs = Board::where('category','like','1')->orderBy('num', 'desc')->paginate(5);
         return view('secretboard.secret-board', compact('msgs'));
     }
+    public function secretModify($num){
+        $msg = Board::find($num);
+        return view('secretboard.secret-modify-form', ['msg' => $msg]);
+    }
+    /////freeboard//////////////////////
     public function secretView($num){
-       //조회수 증가
+       //난수 출력
+        $ran = mt_rand(1, 3);
+        //조회수 증가
         $board = Board::find($num);
         $board->increment('hits');
         $board->save();
        //해당 뷰 보여줌 
         $msg = Board::find($num);
-        return view('secretboard.secret-view', ['msg' => $msg]);
+        return view('secretboard.secret-view', ['msg' => $msg],['ran'=>$ran]);
     }
     public function freeWrite(){
         return view('freeboard.free-write-form');
     }
     public function freeBoard(){
        //페이지네이션
-        $msgs = Board::where('category','like','2')->paginate(5);
+        $msgs = Board::where('category','like','2')->orderBy('num', 'desc')->paginate(5);
         return view('freeboard.free-board',compact('msgs'));
     }
     public function freeView($num){
@@ -72,6 +102,7 @@ class writeController extends Controller
             return redirect('/free/view/'.$num);
         }
     }
+    ///smarteditor///
     public function summernote(){
         $return_value = "";
 
@@ -83,7 +114,7 @@ class writeController extends Controller
                 $destination = 'upload.' . $filename;
                 $location = $_FILES['image']['tmp_name'];#파일 경로
                 move_uploaded_file($location, $destination);
-                $return_value = 'upload.' . $filename;
+                $return_value = 'upload/' . $filename;
             }else{
                 $return_value = '업로드에 실패 하였습니다.: '.$_FILES['image']['error'];
             }
